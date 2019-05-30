1ee6e520d3597e7ca987d2c80dd21ef6de160fc3
Student <example@example.com>
2019-05-30 10:28:47 -0700

    Updated webpack to latest versions, fixed tests to look for Entity instead of Player

--- spec/player-spec.js
+++ spec/player-spec.js
@@ -1,7 +1,8 @@
-import { Player } from '../src/Entity.js/index.js';
-describe('Player', function() {
-  it('should test player constructor', function() {
-    let player1 = new Player("joe");
+import { Entity } from '../src/Entity.js';
+
+describe('Entity', function() {
+  it('should test entity constructor', function() {
+    let player1 = new Entity("joe");
     expect(player1.name).toEqual("joe");
   });
 });
--- src/Entity.js
+++ src/Entity.js
@@ -3,22 +3,22 @@ export class Entity {
   constructor(name){
     this.name = name;
     
-    this.str = 10,// + job.str + race.str,
-    this.dex = 10,// + job.dex + race.dex,
-    this.intel = 10,// + job.intel + race.intel,
-    this.wis = 10,// + job.wis + race.wis,
-    this.luck = 10,// + job.luck + race.luck,
-    this.vit = 10 //+ job.vit + race.vit,
+    this.str = 10,
+    this.dex = 10,
+    this.intel = 10,
+    this.wis = 10,
+    this.luck = 10,
+    this.vit = 10 
 
     this.health = 100 + this.vit;
     this.skillpoints = 100;
-    // this.race = race;
-    // this.job = job;
   }
 
-  // attack(target){
-  //   console.log("attacked");
-  // }
+  attack(target){
+    
+    let roll = Math.random()*20;
+    console.log("attacked");
+  }
 
   dodge(){
 
